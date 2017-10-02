9144a561067f62974de86ad0af973cdf10f0dfde
Eduardo-PC <eduardo.cardoso@outlook.com>
2017-10-02 00:37:56 -0300

    Alterações na tela de pedidos e adição de ações.

---
+++ viewer/home.php
@@ -0,0 +1,10 @@
+<?php
+include_once './cabecalho.php';
+?>
+
+
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+</body>
+
+</html>
